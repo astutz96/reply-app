02bd9c464f91c0a2731f28338e44f7e91212aa46
astutz96 <astutz96.development@protonmail.com>
2023-10-17 19:45:02 -0400

    adding previews for compact, medium, and expanded layous

--- app/src/main/java/com/example/reply/MainActivity.kt
+++ app/src/main/java/com/example/reply/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,14 +46,33 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
+//@Composable
+//fun ReplyAppCompactPreview() {
+//    ReplyTheme {
+//        Surface {
+//            ReplyApp(windowSize = WindowWidthSizeClass.Compact)
+//        }
+//    }
+//}
+
+//@Preview(showBackground = true, widthDp = 700)
+//@Composable
+//fun ReplyAppMediumPreview() {
+//    ReplyTheme {
+//        Surface {
+//            ReplyApp(windowSize = WindowWidthSizeClass.Medium)
+//        }
+//    }
+//}
+
+@Preview(showBackground = true, widthDp = 1000)
 @Composable
-fun ReplyAppCompactPreview() {
+fun ReplyAppExpandedPreview() {
     ReplyTheme {
         Surface {
-            //val windowSize = calculateWindowSizeClass(this)
-            //ReplyApp(windowSize = windowSize.widthSizeClass)
+            ReplyApp(windowSize = WindowWidthSizeClass.Expanded)
         }
     }
 }
+
